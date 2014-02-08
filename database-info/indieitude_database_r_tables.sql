75d3158970f1ffb679cf941c86f87a15fac730be
Amechi Egbe <egbe@usc.edu>
2014-02-08 14:20:28 -0500

    New Database

--- database-info/indieitude_database_r_tables.sql
+++ database-info/indieitude_database_r_tables.sql
@@ -12,8 +12,8 @@ Amechi Egbe
 
 
 create table users (
-  id bigint unsigned not null auto_increment,
-  email varchar(254) unique not null,
+  users_id bigint unsigned not null auto_increment,
+  email varchar(255) unique not null,
   password varchar(255) not null,
   user_points int not null auto_increment,
   user_title varchar(255) auto_increment,
@@ -22,11 +22,9 @@ create table users (
   last_name varchar(255),
   birthday date,
   city varchar(50),
-  state varchar(50),
-  country varchar(30),
-  primary key (id)
+  state varchar(2),
+  primary key (users_id)
 );
-
 
 
 
@@ -36,56 +34,42 @@ create table users (
 --Product Information
 
 create table product (
-  id bigint unsigned not null auto_increment,
-  product_name
-  product_description
-  fiber_types
-  product_types
-  made_in_ny
-  novelty_of_item boolean,
-  foreign key (product_images_id) references product_images(id),
-
-
-
+  product_id bigint unsigned not null auto_increment,
+  product_name varchar(255),
+  product_desc text,
+  color_name varchar(255),
+  size_types varchar(7),
+  fiber_context text,
+  product_price unsigned float,
+  product_tags varchar(255),
+  made_in_ny boolean,
+  novelty_of_item varchar(255),
+  primary key (product_id)
+  foreign key (product_img_id) references product_img(id),
+  foreign key (designer_id) references designer_id,
+  foreign key (product_types) references product_types
 );
+
 
 /*
  * Represents a type of products, e.g. "shirt", accessories or "shoes", increasing uniformity
  * in product table without introducing ambiguity.
  */
 create table product_types (
-  id bigint unsigned not null auto_increment,
+  product_types_id bigint unsigned not null auto_increment,
   name varchar(255),
-  primary key (id)
-);
-
-/*
- * Represents a type of fibers, e.g. "Nylon", accessories or "Cotton", increasing uniformity
- * in product table without introducing ambiguity.
- */
-create table fiber_types (
-  id bigint unsigned not null auto_increment,
-  name varchar(255),
-  primary key (id)
-);
-
-/*
- * The color variations of a single item of a particular product, needing a color variation.
- */
-create table product_colors (
-  id bigint unsigned not null auto_increment,
-  product_id bigint unsigned not null,
-  color_name varchar(255),
-  primary key (id)
+  primary key (product_types_id)
 );
 
 
 --Product Images
 
 create table product_images (
+  product_img_id bigint unsigned not null auto_increment,
+  product_img mediumblob,
   product_id bigint unsigned not null,
   designer_id bigint unsigned not null,
-  primary key (product_id, designer_id),
+  primary key (product_img_id),
   foreign key (product_id) references product(id),
   foreign key (designer_id) references designer(id)
 );
@@ -102,12 +86,11 @@ create table product_likes (
 );
 
 create table product_comments (
-  id bigint unsigned not null auto_increment,
   user_id bigint unsigned not null, /* the comment's author */
   product_id bigint unsigned not null,
   time_posted timestamp,
   body text,
-  primary key (id),
+  primary key (user_id, time_posted),
   foreign key (user_id) references users(id),
   foreign key (product_id) references product(id)
 );
@@ -117,8 +100,31 @@ create table product_comments (
 
 --Designer information
 create table designer (
+designer_id bigint unsigned not null auto_increment,
+designer_name varchar(255),
+designer_labelname varchar(255), 
+designer_phone_number unsigned int,
+designer_email varchar(255),
+designer_address text,
+designer_state varchar(2),
+designer_city varchar(50),
+designer_zipcode unsigned int,
+designer_specialities boolean,
+foreign key (product_id) references product(id)
+foreign key (boutiques_id) references boutiques(id)
+);
 
 
+create table boutiques (
+boutiques_id bigint unsigned not null auto_increment,
+boutique_image mediumblob,
+boutique_name varchar(255),
+boutique_address text,
+boutique_city varchar(50),
+boutique_state varchar(2),
+boutique_zipcode unsigned int,
+foreign key (designer_id) references designer(id),
+foreign key (product_id) references product(id)
 );
 
 
