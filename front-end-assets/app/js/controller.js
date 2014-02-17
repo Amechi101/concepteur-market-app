2b36fd32288f743d91b8d878651df13aaff82aa9
Amechi Egbe <egbe@usc.edu>
2014-02-16 23:45:24 -0500

    Django project

--- front-end-assets/app/js/controller.js
+++ front-end-assets/app/js/controller.js
@@ -1,10 +1,11 @@
+
+
 var indieApp = angular.module('indieApp', []);
 
 indieApp.controller('imageAddCrtl', function($scope, $http) {
-	$http.get('js/imageInfo.json')
-		.then(function(res) {
-			$scope.imageInfo = res.data;
-		});
+	$http.get('js/imageInfo.json').success(function(data) {
+		$scope.imageInfo = data;
+	});
 });
 
 
