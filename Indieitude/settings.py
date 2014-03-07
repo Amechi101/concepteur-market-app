f4383c69578038623b766e4d1de885223b4819c1
Amechi Egbe <egbe@usc.edu>
2014-03-06 23:07:11 -0500

    Switched Project directory around

--- Indieitude/settings.py
+++ Indieitude/settings.py
@@ -81,27 +81,8 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL = '/static/'
-
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
-
+####### For Template Absolute path ###########
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -114,10 +95,11 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
 )
 
 
-######### Heroku Environment #########
+######### Heroku/Django Setup #########
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
@@ -133,9 +115,27 @@ ALLOWED_HOSTS = ['*']
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+)
+
+# Additional locations of static files
+STATICFILES_DIRS = (
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
---
+++ profiles/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+# class ProfileUser(models.Models):
+# 	user = models.OneToOneField(User)
+	
+# 	birthday = models.DateField(null=True, blank=True)
+# 	city = models.CharField(max_length=50, blank=True)
+#     state = models.CharField(max_length=50, blank=True)
+
+#     user_title = models.CharField(max_length=254, verbose_name="Influencer Level", blank=True)
+#     user_points = models.IntegerField(null=False, verbose_name="Influence Credit", blank=True)
+#     picture = moels.ImageField(upload_to='images', blank=True)
+
+#     #admin level and additional infomation
+#     is_active = models.BooleanField(default=True)
+#     is_staff = models.BooleanField(default=False)
+
+#     #Override the _unicode_() methid to return out something meaningful
+#     def _unicode_(self):
+#     	return self.user.username
