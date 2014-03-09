cddcd97b045a130f08fdae0b7e9cb8c933587460
Amechi Egbe <egbe@usc.edu>
2014-03-08 23:45:16 -0500

    change front-end assets and django

--- Indieitude/urls.py
+++ Indieitude/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
+import profiles
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'indieitudeapp.views.home', name='home'),
+    # url(r'^$', 'indieitude.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^profiles/', include(profiles.urls)),
 )
