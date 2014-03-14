b508d1283fc6826ab58fdf23ebba1955c9607398
Amechi Egbe <egbe@usc.edu>
2014-03-14 10:59:37 -0400

    gone from settings.py

--- Indieitude/settings.py
+++ Indieitude/settings.py
@@ -36,8 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.seretballot',
-    'django.conrtib.likes',
 
     ##### Indieitude Apps #####
     'home', #homepage
