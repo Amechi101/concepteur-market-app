3d00b1d92cc97f8de3ddcc1b6fb17b6054cfb33b
Amechi Egbe <egbe@usc.edu>
2014-03-14 10:47:14 -0400

    deleted from urls.py

--- Indieitude/urls.py
+++ Indieitude/urls.py
@@ -11,13 +11,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-<<<<<<< HEAD
     url(r'^home/', 'home.views.base', name="base"),
     url(r'^profiles/', 'profiles.views.login', name="login"),
-=======
-    # url(r'^profiles/', include('profiles.urls')),
-    url(r'^$', 'home.views.index'),
-    url(r'^likes/', include('likes.urls')),
->>>>>>> FETCH_HEAD
 )
 
